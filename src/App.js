a5ce925cc9b763e996b0e4582f6ce6fcdd6aed58
Avijit Karmaker <ajkacca@gmail.com>
2021-02-05 22:56:23 +0200

    add linters setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-
-const App= ()=> {
-  return (
-    <div className="App">
-      <h1 style={{color:"white", fontSize:"200%"}}>Redux-Bookstore</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1 style={{ color: 'white', fontSize: '200%' }}>Redux-Bookstore</h1>
+  </div>
+);
 
 export default App;
