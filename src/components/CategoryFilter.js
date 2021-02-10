b46afaa8e26c6aefd05d9fddc9909e18a359e266
Avijit Karmaker <ajkacca@gmail.com>
2021-02-10 04:25:41 +0200

    add styling, fix linters

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -3,16 +3,27 @@ import PropTypes from 'prop-types';
 import { categories } from '../data/appdata';
 
 const Filter = ({ handleFilterChange }) => (
-  <select
-    id="filter"
-    onChange={e => handleFilterChange(e.target.value)}
-  >
-    {['All', ...categories].map(category => (
-      <option key={category} value={category}>
-        {category}
-      </option>
-    ))}
-  </select>
+  <div className="filtercomp" style={{ textAlign: 'center' }}>
+    <h3 style={{
+      color: 'white',
+      textDecoration: 'underline',
+      marginBottom: '5px',
+    }}
+    >
+      Find books by categories:
+    </h3>
+    <select
+      id="filter"
+      onChange={e => handleFilterChange(e.target.value)}
+      style={{ width: '30%', textAlign: 'right' }}
+    >
+      {['All', ...categories].map(category => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  </div>
 );
 
 Filter.propTypes = {
